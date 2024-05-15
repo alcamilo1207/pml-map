4180914656dc342c5c2f11f57e0af73a24948cbc
alcamilo1207 <81839026+alcamilo1207@users.noreply.github.com>
2024-05-15 14:55:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -22,8 +22,7 @@ def main():
         # Display the map
         fig = px.scatter_mapbox(df,lat="Latitude",lon="Longitude",hover_name="Name",size="total_miembros",mapbox_style="dark",zoom=10)
         fig.update_layout(height=600)
-        fig.update_traces(marker=dict(color='red',
-                                      line=dict(width=2,color='orange')))
+        fig.update_traces(marker=dict(size=12,line=dict(width=2,color='DarkSlateGrey')),selector=dict(mode='markers'))
         st.plotly_chart(fig,use_container_width=True,heigth=600)
     else:
         st.subheader("Data")
